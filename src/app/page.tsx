7bd2c8330c26f211c05845bc2ed8dc278d1058e4
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2025-02-26 15:02:42 +0000

    update login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="login"><button>Login</button></Link>
+        <Link href="dashboard"><button>Login</button></Link>
       </main>
     </div>
   );
