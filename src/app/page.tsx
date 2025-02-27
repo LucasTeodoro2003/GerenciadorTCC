83abbb3d2afbeed00169cb3ec3ee04dab161aff6
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2025-02-27 22:31:01 +0000

    update bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="dashboard"><button>Login</button></Link>
-      </main>
-    </div>
-  );
+  redirect("/dashboard");
+
+  // return (
+  //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  //     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+  //       <Link href="dashboard">
+  //         <button>Login</button>
+  //       </Link>
+  //     </main>
+  //   </div>
+  // );
 }
