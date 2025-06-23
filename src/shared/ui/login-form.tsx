d0d3ca317be3b1cca9653c66de5f73253892f7fb
LucasTeodoro2003 Teodoro <lucasteodoro.lm@gmail.com>
2025-06-23 14:02:39 +0000

    update login and signOut

--- src/shared/ui/login-form.tsx
+++ src/shared/ui/login-form.tsx
@@ -10,6 +10,7 @@ import { LoginErrorMessage } from "@/features/actions/login/errorMensagens";
 import { Suspense } from "react";
 import { useFormStatus } from "react-dom";
 import { SignUpGoogle } from "@/app/api/auth/callback/google";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export function LoginForm({
   className,
@@ -19,12 +20,15 @@ export function LoginForm({
     const { pending } = useFormStatus();
 
     return (
-      <Button type="submit" className="w-full" disabled={pending}>
-        {pending ? "Carregando..." : "Login"}
+      <Button
+        type="submit"
+        className={cn("w-full", pending && "flex items-center justify-center")}
+        disabled={pending}
+      >
+        {pending ? <CircularProgress size={20} /> : "Login"}
       </Button>
     );
   }
-
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
@@ -94,7 +98,11 @@ export function LoginForm({
                   </svg>
                   <span className="sr-only">Login with Meta</span>
                 </Button>
-                <Button variant="outline" className="w-full" onClick={SignUpGoogle}>
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  onClick={SignUpGoogle}
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
--- src/shared/ui/signOut.tsx
+++ src/shared/ui/signOut.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 import { signOut as signOutFn } from "next-auth/react";
 
-export default async function signOut() {
-  await signOutFn();
-  redirect("/login");
+export default function signOut() {
+  signOutFn({ callbackUrl: "/login" });
 }
