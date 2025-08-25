affc7157a29b0637d93912a44ecaea98c2d3288b
LucasTeodoro2003 Teodoro <lucasteodoro.lm@gmail.com>
2025-08-25 14:06:13 +0000

    update dakmode button

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -13,11 +13,12 @@ export default async function LoginPage() {
 
   return (
     <>
+      <div className="absolute top-6 right-6">
+        <ThemeToggleV2 />
+      </div>
+      
       <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
         {/* <ThemeToggle /> */}
-        <div className="ml-auto">
-        <ThemeToggleV2/>
-        </div>
         <div className="w-full max-w-sm md:max-w-3xl">
           <LoginForm />
         </div>
