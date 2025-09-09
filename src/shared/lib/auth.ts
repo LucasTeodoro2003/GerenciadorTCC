9f1ed03381edb95dd86c11f2e8ab146df408065f
LucasTeodoro2003 Teodoro <lucasteodoro.lm@gmail.com>
2025-09-09 22:31:17 +0000

    update auth

--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth, { CredentialsSignin } from "next-auth";
 import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google"
+import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import db from "./prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { schema } from "./schema";
 import { v4 as uuid } from "uuid";
 import { encode } from "@auth/core/jwt";
@@ -32,7 +32,7 @@ export const { handlers, auth, signIn } = NextAuth({
         if (!user || !user.password) {
           throw new CredentialsSignin("Usuário não encontrado!");
         }
-        const valid = bcrypt.compareSync(validateCredentials.password, user.password)
+        const valid = bcrypt.compareSync(validateCredentials.password, user.password);
         if (!valid) {
           throw new CredentialsSignin("Senha incorreta!");
         }
@@ -41,6 +41,40 @@ export const { handlers, auth, signIn } = NextAuth({
     }),
   ],
   callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account?.provider && account.provider !== "credentials") {
+        const existingUser = await db.user.findFirst({
+          where: { email: user.email },
+        });
+        if (existingUser) {
+          const existingAccount = await db.account.findFirst({
+            where: {
+              userId: existingUser.id,
+              provider: account.provider,
+            },
+          });
+
+          if (!existingAccount) {
+            await db.account.create({
+              data: {
+                userId: existingUser.id,
+                type: account.type,
+                provider: account.provider,
+                providerAccountId: account.providerAccountId,
+                refresh_token: account.refresh_token,
+                access_token: account.access_token,
+                expires_at: account.expires_at,
+                token_type: account.token_type,
+                scope: account.scope,
+                id_token: account.id_token,
+              },
+            });
+          }
+          user.id = existingUser.id;
+        }
+      }
+      return true;
+    },
     async jwt({ token, account }) {
       if (account?.provider === "credentials") {
         token.credentials = true;
@@ -58,7 +92,7 @@ export const { handlers, auth, signIn } = NextAuth({
         const createdSession = await adapter?.createSession?.({
           sessionToken: sessionToken,
           userId: params.token.sub,
-          expires: new Date(Date.now() + 10 * 60 * 1000)
+          expires: new Date(Date.now() + 10 * 60 * 1000),
         });
 
         if (!createdSession) {
