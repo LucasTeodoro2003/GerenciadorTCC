ec6c8617d90e2c66db7a4a84284ce62cf2b1a795
LucasTeodoro2003 Teodoro <lucasteodoro.lm@gmail.com>
2025-09-17 12:34:58 +0000

    update actionUpdateRevenue

--- src/shared/lib/actionUpdateRevenue.ts
+++ src/shared/lib/actionUpdateRevenue.ts
@@ -7,6 +7,13 @@ export async function updateRevenue(id: string, formData: FormData) {
   try {
     const serviceVehicle = await db.serviceVehicle.findUnique({
       where: { id },
+      include: {
+        services: {
+          include: {
+            service: true
+          }
+        }
+      }
     });
 
     if (serviceVehicle) {
@@ -18,20 +25,34 @@ export async function updateRevenue(id: string, formData: FormData) {
         },
       });
       
-      if (serviceVehicle.serviceId) {
-        await db.services.update({
-          where: { id: serviceVehicle.serviceId },
-          data: {
-            description: formData.get("description")?.toString() || "",
-          },
-        });
+      const descriptionUpdate = formData.get("description")?.toString();
+      
+      if (descriptionUpdate && serviceVehicle.services.length > 0) {
+        const firstServiceRelation = serviceVehicle.services[0];
+        
+        if (firstServiceRelation && firstServiceRelation.service) {
+          await db.services.update({
+            where: { id: firstServiceRelation.serviceId },
+            data: {
+              description: descriptionUpdate,
+            },
+          });
+        }
+        for (const serviceRelation of serviceVehicle.services) {
+          await db.services.update({
+            where: { id: serviceRelation.serviceId },
+            data: {
+              description: descriptionUpdate,
+            },
+          });
+        }
       }
     } else {
       await db.revenue.update({
         where: { id },
         data: {
           amount: formData.get("amount")?.toString() || "",
-          date: new Date(formData.get("date")?.toString() || new Date().toISOString()), // Correção em uma linha
+          date: new Date(formData.get("date")?.toString() || new Date().toISOString()),
           description: formData.get("description")?.toString() || "",
           category: formData.get("category")?.toString() || "",
           source: formData.get("source")?.toString() || "",
